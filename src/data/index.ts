ade90f8dec0eb99887bfc2d78127537501f0a7fb
Anish <anish.maharjan@zendesk.com>
2023-10-14 10:51:05 +1100

    feat(data): add mock data

---
+++ src/data/index.ts
@@ -0,0 +1,58 @@
+import { BarChartData } from '@/types'
+
+export const mockData: BarChartData[] = [
+  {
+    id: 1,
+    frequency: 10
+  },
+  {
+    id: 2,
+    frequency: 5
+  },
+  {
+    id: 3,
+    frequency: 14
+  },
+  {
+    id: 4,
+    frequency: 8
+  }
+]
+
+export const mockData1: BarChartData[] = [
+  {
+    id: 1,
+    frequency: 1
+  },
+  {
+    id: 2,
+    frequency: 5
+  },
+  {
+    id: 3,
+    frequency: 10
+  },
+  {
+    id: 4,
+    frequency: 7
+  }
+]
+
+export const mockData2: BarChartData[] = [
+  {
+    id: 1,
+    frequency: 20
+  },
+  {
+    id: 2,
+    frequency: 15
+  },
+  {
+    id: 3,
+    frequency: 14
+  },
+  {
+    id: 4,
+    frequency: 4
+  }
+]
