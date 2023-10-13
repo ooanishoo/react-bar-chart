cc78e516a51a90ecab78c0b0a18b047be06d3c87
Anish <anish.maharjan@zendesk.com>
2023-10-14 10:54:41 +1100

    feat(App): render multiple bar charts from App component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,30 @@
 import { ReactElement } from 'react'
+import styled from 'styled-components'
+import BarChartContainer from '@/components/BarChartContainer'
+import { mockData1, mockData2 } from '@/data'
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`
 
 const App = (): ReactElement => {
-  return <>{'React Bar Chart'}</>
+  return (
+    <StyledWrapper>
+      <BarChartContainer />
+      <BarChartContainer data={mockData1} />
+      <BarChartContainer
+        data={mockData2}
+        dimension={{
+          width: 700,
+          height: 500
+        }}
+      />
+    </StyledWrapper>
+  )
 }
 
 export default App
